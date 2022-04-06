26241cb60f41afe0b9e5de00d443003a91bc8da7
eemanramzi <eemanramzi@gmail.com>
2022-04-06 16:41:38 -0400

    Update import files.py

--- import files.py
+++ import files.py
@@ -1,4 +1,7 @@
 import pandas as pd
 import csv
-from pandas import read_csv
-df = pd.read_csv(r'C:\Users\mimil\Desktop\School\Third Year\Second Semester\CHE305 - Lab\Lab 11\CHE305-Lab-11\w5.csv')
+# from pandas import read_csv
+# df = pd.read_csv(r'C:\Users\mimil\Desktop\School\Third Year\Second Semester\CHE305 - Lab\Lab 11\CHE305-Lab-11\w5.csv')
+from os import listdir
+from os.path import isfile,join 
+files = [f for f in listdir('/Users/eemanabdulkadir/Documents/GitHub/CHE305-Lab-11/Data/Refrigerant=0.7 kg') if isfile(join('/Users/eemanabdulkadir/Documents/GitHub/CHE305-Lab-11/Data/Refrigerant=0.7 kg', f))]
