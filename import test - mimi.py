d3d6ecdcfa038abd294c7cccc855847eed49f237
Mimi <mimile246@gmail.com>
2022-04-08 17:58:34 -0400

    Update import test - mimi.py

--- import test - mimi.py
+++ import test - mimi.py
@@ -26,3 +26,42 @@ def read_r7(path):
 
     df = pd.concat(li, axis=0, ignore_index=True)
     return df
+
+def read_r8(path):
+    path = r'https://github.com/meai2/CHE305-Lab-11/tree/main/Data/Refrigerant%3D0.8%20kg'
+    all_files = glob.glob(path + "/*.csv")
+
+    li = []
+
+    for filename in all_files:
+        df = pd.read_csv(filename, index_col=None, header=0)
+        li.append(df)
+
+    df = pd.concat(li, axis=0, ignore_index=True)
+    return df
+
+def read_r88(path):
+    path = r'https://github.com/meai2/CHE305-Lab-11/tree/main/Data/Refrigerant%3D0.88%20kg'
+    all_files = glob.glob(path + "/*.csv")
+
+    li = []
+
+    for filename in all_files:
+        df = pd.read_csv(filename, index_col=None, header=0)
+        li.append(df)
+
+    df = pd.concat(li, axis=0, ignore_index=True)
+    return df
+
+def read_r1(path):
+    path = r'https://github.com/meai2/CHE305-Lab-11/tree/main/Data/Refrigerant%3D1%20kg'
+    all_files = glob.glob(path + "/*.csv")
+
+    li = []
+
+    for filename in all_files:
+        df = pd.read_csv(filename, index_col=None, header=0)
+        li.append(df)
+
+    df = pd.concat(li, axis=0, ignore_index=True)
+    return df
