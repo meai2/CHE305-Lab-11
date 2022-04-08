81c86abe55b41478528df20ddf6e5d1fd5d3493d
Mimi <mimile246@gmail.com>
2022-04-08 18:04:25 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
 import pandas
+from pandas import read_csv
+
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
+
+print(dataset.describe())
