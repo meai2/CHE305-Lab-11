c4cc12738bedf4d974a10d5499640198ae8f9faf
Jover1212 <jackovery@gmail.com>
2022-04-11 21:09:38 -0400

    Revert "Revert "Merge branch 'main' of https://github.com/meai2/CHE305-Lab-11""
    This reverts commit ecf58b9ecc1573ea5695875cd429e87693463c7d.

--- import files.py
+++
@@ -1,2 +0,0 @@
-import pandas as pd
-df = pd.read_csv(r'C:\Users\mimil\Desktop\School\Third Year\Second Semester\CHE305 - Lab\Lab 11\CHE305-Lab-11\w5.csv')
--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-print('hello')
-print('hello back')
+import pandas as pd
+import numpy as np
+from sklearn.datasets import load_iris
+from IPython.display import display
+
+data = load_iris()
+df = pd.DataFrame(data.data, columns = data.feature_names)
+
+display(df.to_string())
