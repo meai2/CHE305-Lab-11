1d38f156e2146057874c908ca71136f89ea2ddee
kusmicar <102686321+kusmicar@users.noreply.github.com>
2022-04-13 19:26:23 -0400

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-from sklearn.datasets import load_iris
-from IPython.display import display
-
-data = load_iris()
-df = pd.DataFrame(data.data, columns = data.feature_names)
-
-display(df.to_string())
